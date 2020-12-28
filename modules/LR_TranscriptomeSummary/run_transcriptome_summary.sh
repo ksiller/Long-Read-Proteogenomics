a15450388be526cbc72047dff6ff7c1cc19d9d59
bj8th <bj8th@virginia.edu>
2020-12-28 12:07:58 -0500

    transcriptome_summary bash

---
+++ modules/LR_TranscriptomeSummary/run_transcriptome_summary.sh
@@ -0,0 +1,9 @@
+transcriptome_summary.py \
+--sq_out $sqanti_classification \
+--tpm $tpm \
+--ribo $ribo \
+--ensg_to_gene $ensg_to_gene \
+--enst_to_isoname $enst_to_isoname
+--len_stats $len_stats \
+--odir summary_results
+
